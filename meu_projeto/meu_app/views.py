13d3954aa35a6c4862c2dd7336b6a2a1cf94b779
Ataide-Araujo <ataide.nascimento@nappsolutions.com>
2022-07-16 12:28:47 -0300

    add def de calculos

--- meu_projeto/meu_app/views.py
+++ meu_projeto/meu_app/views.py
@@ -5,4 +5,19 @@ from django.shortcuts import render, HttpResponse
 
 def hello(request, nome, idade):
     return HttpResponse(f'<h1>Hello {nome} de {idade} anos</h1>')
-    
+
+def soma(request, num1:int, num2:int):
+    soma = num1 + num2
+    return HttpResponse(f'<h1>{num1} + {num2} = {soma}</h1>')
+
+def subtrai(request, num1:int, num2:int):
+    sub = num1 - num2
+    return HttpResponse(f'<h1>{num1} - {num2} = {sub}</h1>')
+
+def multiplica(request, num1:int, num2:int):
+    mult = num1 * num2
+    return HttpResponse(f'<h1>{num1} * {num2} = {mult}</h1>')
+
+def divide(request, num1:int, num2:int):
+    div = num1 / num2
+    return HttpResponse(f'<h1>{num1} / {num2} = {div}</h1>')
